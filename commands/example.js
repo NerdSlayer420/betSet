2ec05c3bcf0ba53131b4839635cf61a31b8c6ccc
NerdSlayer420 <76216719+NerdSlayer420@users.noreply.github.com>
2020-12-18 12:57:35 -0400

    Update example.js

--- commands/example.js
+++ commands/example.js
@@ -7,16 +7,19 @@
 "use strict";
 
 const tesseract = require("@bastion/tesseract");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
 
 class ExampleCommandPlugin extends tesseract.Command {
     constructor() {
-        super("example", {
-            description: "This is an example command. It logs the Command object, the message that triggered the command, and the argumens passed to this command.",
-            triggers: [ "ex", "eg" ],
+        super("betSet", {
+            description: "This is the first plugin that I have attemped. It's a simple bet system where you can make an open bet that anyone on your server may use to initiate a wager",
+            triggers: [ "betSet", "bs" ],
             arguments: {},
             scope: "guild",
             owner: false,
-            typing: false,
+            typing: true,
             schedulable: true,
             unsafe: false,
             nsfw: false,
@@ -28,6 +31,16 @@ class ExampleCommandPlugin extends tesseract.Command {
             condition: () => true,
         });
     }
+
+
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	// ...
+});
 
     exec = async (message, argv) => {
         console.log("COMMAND: ", this);
